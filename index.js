aa9dfef11c15d89da2cf3e7b3ae316cb1c706a41
Manu <monkey@donkey.com>
2024-09-10 20:24:14 -0300

    fix dinamic cards

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const h1 = document.querySelector(".body-title");
+const h1 = document.querySelector("h1");
+
+const section = document.querySelector("section");
 
 h1.innerText = "Productos";
 
-const array = [];
+let array = [];
 
-for (i = 1; i < 9; i++){
-
-    // array.push(`<img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg"`);
+for (let i = 1; i < 10; i++){
 
     array.push(`<div class="card">
                     <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="Nike Air Zoom Pegasus 39 Shield">
                     <div class="card-body">
-                        <h5 class="card-title">Nike Air Zoom Pegasus 39 Shield</h5>
+                        <h5 class="card-title">Car ${i}</h5>
                         <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit donec, eleifend aenean purus dui ornare blandit risus facilisis interdum, cum nostra vulputate sem venenatis felis facilisi.</p>
                         <p class="card-price">$2.100</p>
                         <button type="button" class="btn btn-outline-dark">Buy item</button>
                         <p class="card-text"><small class="text-body-secondary">Last updated 32 mins ago</small></p>
                     </div>
-                </div>`)
+                </div>`);
 
-    document.querySelector(".card-group").innerHTML = (array);
 }
+
+section.innerHTML = array.join().replaceAll(",", ""); //unión del array a string, y se eliminan las comas
