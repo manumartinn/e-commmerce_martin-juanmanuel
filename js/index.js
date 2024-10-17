715b1fe752dbc32a3575a3413b1f090d0a16ec5e
Manu <monkey@donkey.com>
2024-10-17 18:50:29 -0300

    corrections sprint 2

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ const mostrarCards = (autos) => {
             </div>
             <div class="card-body">
                 <h5 class="card-title">${auto.modelo}</h5>
+                <h6>${auto.categoria}</h6>
                 <p class="card-text">${auto.descripcion.split(0,40)}...</p>
                 <p class="card-price">$${auto.precio}</p>
                 <button type="button" class="btn btn-outline-dark custom-btn-white"><a class="button-link" href="../pages/producto.html?prod=${auto.id}">See more</a></button>
